0990aefaab509f3ec3a5b467b03cc6f1517ccfa9
samfeldman824 <sam@feldman.com>
2022-10-10 17:04:59 -0400

    changed encoder level to 5.1

--- production.sh
+++ production.sh
@@ -88,7 +88,7 @@ compression() {
       if [[ $i != *"files to delete"* ]]; then
       cd "$( realpath "$i" )"
       folder="${PWD##*/}"
-      HandBrakeCLI -i "${folder}".MP4 -o "${folder}cp".MP4 --preset "Very Fast 1080p30" -r 60.0 -q 22.0
+      HandBrakeCLI -i "${folder}".MP4 -o "${folder}cp".MP4 --preset "Very Fast 1080p30" -r 60.0 -q 22.0 --encoder-level 5.1
       # HandBrakeCLI -i "${folder}".MP4 --preset-import-file [PATH to JSON] -o "${folder} (compressed)".MP4
       fi
   done
